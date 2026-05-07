cf0e80ea505a7646e1a2fd9213b317274fae650b
Roman Plyska <rplyska@gmail.com>
2026-05-07 10:17:39 +0300

    Add files via upload

---
+++ lesson-1/script/js.js
@@ -0,0 +1,47 @@
+// console.log("helo piton");
+// console.info("helo 2");
+// console.error("helo 3");
+// console.warn("helo 4");
+//
+
+// var MyFirstVar;
+// MyFirstVar = 10;
+// console.log(MyFirstVar);
+// var MySecondVar = "2";
+// console.log("zminna: " + MySecondVar);
+// console.log(MySecondVar + MyFirstVar);
+// console.log(MySecondVar - MyFirstVar);
+// // var fuhrst, secomd = 1, 2;
+// var int = 10;
+// var boule = true;
+// var str = "str";
+// var nuthimg;
+// nothimg = null;
+//
+//
+// let a = 2;
+// var b = 12;
+// console.log(num)
+// console.log(a+b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+//
+// let a, b;
+// a = 2+4;
+// b = 3 + (a = 10 + 3)
+// a = a+1;
+// a +=1;
+// a++;
+// let a = 2, b;
+// b = a++;
+// console.log(a, b);
+//
+// let a = "2", b = "3";
+// console.log((a-0)+(b-0));
+// // console.log(Number(a)+ Number(b));
+let name = "Ivan"
+let skill = "web"
+console.log("Priv " + name + `! yor skil is sue is: ${skill} ${2*4}`)
+
