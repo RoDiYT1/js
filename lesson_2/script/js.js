2067eddc167b47b4f5d0c8172d8f3373b91983df
Roman Plyska <rplyska@gmail.com>
2026-05-11 13:54:13 +0300

    Add files via upload

---
+++ lesson_2/script/js.js
@@ -0,0 +1,74 @@
+// false
+// 0
+// 0n
+//
+// null
+// undefined
+// NaN
+//
+// if(umova){
+//     [insr if istyna];
+//     [instr 2]
+// }
+// else if{
+//     [instr]
+// }
+// else{
+//     []
+// }
+// ==
+// !=
+// >
+// <
+// >=
+// <=
+// ===
+// !==
+//
+// if a=3 b="3";
+// console.log(a===b)
+// let a = prompt("1 chysl");
+// let b = prompt("2 chysl");
+// let c;
+// if(a>b){
+//     c="a>b"
+// }else if(a<b){
+//     c="a<b"
+//
+// }else{
+//     c="a==b"
+// }
+// alert(c)0
+//
+// let course = prompt("kurs pls nam")
+// switch(course){
+//     case "figma":
+//         title = "WEB rozrobbaka"
+//         break;
+//     case "pyton":
+//         title = "progruming"
+//         break;
+//     case "frontend":
+//     case "backend":
+//         title = "WEB rozrobbaka"
+//         break;
+// }
+// alert(title);
+//
+// //logopp
+// let a = false, b = false, c=true;
+// console.log(!a);
+// console.log(!!a)
+//
+// let age=prompt("how ol r u"), info;
+// if (age<18){
+//     info="shkilota"
+// }
+// else if (age>18&&age<35){
+//     info="prizivnik busifikatsiya"
+// }else{
+//     info="ye chanses"
+// }
+// let name = prompt("wat iz yo name");
+//     greeding = `hi ${name || "gist"}!`;
+//     alert(greeding);
